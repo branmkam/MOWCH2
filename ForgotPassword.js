1b993f7a066aa3d53541340a34f3f4aebf3ab0e7
branmkam <54331071+branmkam@users.noreply.github.com>
2020-12-10 14:31:15 -0500

    added forgot password email screen
    figure out how to send reset password link

--- ForgotPassword.js
+++ ForgotPassword.js
@@ -19,26 +19,12 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 export default function Login2({ navigation }) {
   const [email, setEmail] = useState(""); // the variable email will have whatever the user inputted
-  const [password, setPassword] = useState(""); // the variable password will have whatever the user inputted
   const [errorMessage, setErrorMessage] = useState("");
 
-  const logIn = (e) => {
+  const sendRecovery = (e) => {
     e.preventDefault();
-    fireAuth
-      .signInWithEmailAndPassword(email, password)
-      .then((auth) => {
-        if (auth) {
-          // there is a user so navigate to home screen
-          //console.log(auth.user);  WORKS
-          navigation.navigate("Home");
-          // navigation.reset({
-          //   index: 0,
-          //   routes: [{name: 'Home'}],
-          // });
-        }
-      })
-      .catch((err) => setErrorMessage(err.message)); // an error messsage that displays underneath
-    // firebase log in here
+    //fireAuth etc
+    // firebase check if email present, then send email
   };
 
   return (
@@ -51,45 +37,14 @@ export default function Login2({ navigation }) {
           onChangeText={(e) => setEmail(e)}
           placeholder="Email"
         />
-
-        <TextInput
-          id="login-password"
-          style={styles.input_container}
-          onChangeText={(e) => setPassword(e)}
-          placeholder="Password"
-          secureTextEntry={true}
-        />
-
-        <Text style={styles.error_message}>{errorMessage}</Text>
-
-        <Text
-          id="login-forgot"
-          style={styles.forgot_password}
-          onPress = {() => navigation.navigate("ForgotPassword")}
-          // TODO navigate to forgot password page
-        >
-          Forgot Password?
-        </Text>
         <Button
-          id="login-button"
-          title="Log In"
+          id="recovery-button"
+          title="Send Recovery Email"
           color="#00B7C4"
-          onPress={logIn}
+          onPress={() => 0} // TODO send recovery email
           width="100%"
           height="100%"
         />
-      </View>
-      <View>
-        <Text id="login-donthave" style={styles.dont_have_account}>
-          Don't have an account?
-        </Text>
-        <Text
-          id="login-signuptext"
-          style={styles.sign_up_text}
-          onPress={() => navigation.navigate("Signup")} //set this as a hyperlink to the Signup page
-        >
-          Sign Up
-        </Text>
       </View>
     </SafeAreaView>
   );
@@ -118,6 +73,18 @@ const styles = StyleSheet.create({
   },
 
   login_button: {
+    fontFamily: Montserrat_400Regular,
+    backgroundColor: "white",
+    fontSize: "140%",
+    width: "100%",
+    height: "100%",
+    marginBottom: "1%",
+    marginTop: "1%",
+    borderRadius: "25px",
+    paddingLeft: "1%",
+  },
+
+  recovery_button: {
     fontFamily: Montserrat_400Regular,
     backgroundColor: "white",
     fontSize: "140%",
